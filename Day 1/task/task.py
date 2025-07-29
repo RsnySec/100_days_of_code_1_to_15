65fba3081283bbc2f15a326f9c013515ee19da01
RsnySec <bensny88@gmail.com>
2025-07-28 21:26:51 -0600

    End of day 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Next Generation Band Name Generator")
+city = input("What city did you grow up in?\n")
+petname = input("What is your pet's name\n")
+print("Your band name could be " + city + " " + petname)
