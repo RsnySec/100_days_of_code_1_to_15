2a622b52b5e6a87c53175eb4dba2b5f6ef81019c
RsnySec <bensny88@gmail.com>
2025-08-19 14:00:04 -0600

    End of day 2.1

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,4 +1,13 @@
-bmi = 84 / 1.65 ** 2
-print(int(bmi))
-print(round(bmi))
-print(round(bmi, 1))
+score = 0
+
+score += 1
+
+print(score)
+
+# f-strings
+
+height = 1.8
+
+is_winning = True
+
+print(f"I am = {score}, your height is {height}. You are winning is {is_winning}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+you_owe = round((bill / people) * (tip / 100) + (bill / people), 2)
 
+print(f"You owe ${you_owe}")
