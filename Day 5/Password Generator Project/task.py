7ee34d17c757bf9e9402245c2975c858b838bdb3
RsnySec <bensny88@gmail.com>
2025-08-24 09:59:16 -0600

    End of day 2.1

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -7,4 +7,26 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Easy
+import random
 
+password_list = []
+
+for char in range(0 ,nr_letters):
+    password_list.append(random.choice(letters))
+
+
+for num in range(0 , nr_numbers):
+    password_list.append(random.choice(numbers))
+
+for sym in range(0 , nr_symbols):
+    password_list.append(random.choice(symbols))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+print(password)
